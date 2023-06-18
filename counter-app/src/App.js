e584b9ef062b071278ebe0d6fb7e45392e22560d
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-18 10:00:25 +0530

    initial setup

---
+++ counter-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import {useSelector} from 'react-redux'
+
+function App() {
+  const counter = useSelector()
+  return (
+    <div>
+      <h1>Counter App</h1>
+    </div>
+  );
+}
+
+export default App;
